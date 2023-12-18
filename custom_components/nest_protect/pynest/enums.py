af5ec4da4b57306df052671612c1a49cca93ccc6
Mick Vleeshouwer <mick@imick.nl>
2023-12-18 20:32:16 +0000

    Add more bucket types

--- custom_components/nest_protect/pynest/enums.py
+++ custom_components/nest_protect/pynest/enums.py
@@ -10,14 +10,35 @@ class BucketType(StrEnum):
     """Bucket types."""
 
     BUCKETS = "buckets"
+    DELAYED_TOPAZ = "delayed_topaz"
+    DEMAND_RESPONSE = "demand_response"
     DEVICE = "device"
+    DEVICE_ALERT_DIALOG = "device_alert_dialog"
+    GEOFENCE_INFO = "geofence_info"
     KRYPTONITE = "kryptonite"  # Temperature Sensors
+    LINK = "link"
+    MESSAGE = "message"
+    MESSAGE_CENTER = "message_center"
+    METADATA = "metadata"
+    OCCUPANCY = "occupancy"
     QUARTZ = "quartz"
     RCS_SETTINGS = "rcs_settings"
+    SAFETY = "safety"
+    SAFETY_SUMMARY = "safety_summary"
+    SCHEDULE = "schedule"
     SHARED = "shared"
     STRUCTURE = "structure"  # General
+    STRUCTURE_HISTORY = "structure_history"
+    STRUCTURE_METADATA = "structure_metadata"
     TOPAZ = "topaz"  # Nest Protect
+    TOPAZ_RESOURCE = "topaz_resource"
     TRACK = "track"
+    TRIP = "trip"
+    TUNEUPS = "tuneups"
+    USER = "user"
+    USER_ALERT_DIALOG = "user_alert_dialog"
+    USER_SETTINGS = "user_settings"
+    WIDGET_TRACK = "widget_track"
     WHERE = "where"  # Areas
 
     UNKNOWN = "unknown"
